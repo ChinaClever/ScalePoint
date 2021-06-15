06ee75555699326b9cf7d4b697fdade5577c0842
luozhiyong <luozhiyong131@qq.com>
2021-06-15 17:05:24 +0800

    牛逼的主界面

--- IMM-CalibrateTool/IMM-CalibrateTool/mainwindow.cpp
+++ IMM-CalibrateTool/IMM-CalibrateTool/mainwindow.cpp
@@ -6,6 +6,9 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    mNavBarWid = new NavBarWid(ui->barWid);
+    QTimer::singleShot(50,this,SLOT(initFunSlot())); //延时初始化
+    connect(mNavBarWid, SIGNAL(navBarSig(int)), this, SLOT(navBarSlot(int)));
 }
 
 MainWindow::~MainWindow()
@@ -13,3 +16,27 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::initFunSlot()
+{
+    initWid();
+}
+
+void MainWindow::initWid()
+{
+    mHomeWid = new Home_MainWid(ui->stackedWid);
+    ui->stackedWid->addWidget(mHomeWid);
+
+    //mSet = new Set_MainWid(ui->stackedWid);
+    //ui->stackedWid->addWidget(mSet);
+
+    mSetupWid = new Setup_MainWid(ui->stackedWid);
+    ui->stackedWid->addWidget(mSetupWid);
+
+    mLog = new LogMainWid(ui->stackedWid);
+    ui->stackedWid->addWidget(mLog);
+}
+
+void MainWindow::navBarSlot(int id)
+{
+    ui->stackedWid->setCurrentIndex(id);
+}
--- IMM-CalibrateTool/IMM-CalibrateTool/mainwindow.h
+++ IMM-CalibrateTool/IMM-CalibrateTool/mainwindow.h
@@ -2,6 +2,11 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include "navbarwid.h"
+//#include "set_mainwid.h"
+#include "logmainwid.h"
+#include "setup_mainwid.h"
+#include "home_mainwid.h"
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -15,7 +20,20 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+protected:
+    void initWid();
+
+protected slots:
+    void initFunSlot();
+    void navBarSlot(int);
+
 private:
     Ui::MainWindow *ui;
+
+    LogMainWid *mLog;
+    //Set_MainWid *mSet;
+    NavBarWid *mNavBarWid;
+    Home_MainWid *mHomeWid;
+    Setup_MainWid *mSetupWid;
 };
 #endif // MAINWINDOW_H
