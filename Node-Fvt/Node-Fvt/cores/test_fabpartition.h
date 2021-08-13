eb0431a161f2d73af31d9b225180313699edae91
luozhiyong <luozhiyong131@qq.com>
2021-08-13 19:15:00 +0800

    fab

---
+++ Node-Fvt/Node-Fvt/cores/test_fabpartition.h
@@ -0,0 +1,30 @@
+#ifndef TEST_FBAPARTITION_H
+#define TEST_FBAPARTITION_H
+
+#include "test_serialnumber.h"
+
+class test_FabPartition : public BaseThread
+{
+    Q_OBJECT
+    explicit test_FabPartition(QObject *parent = nullptr);
+public:
+    static test_FabPartition *bulid(QObject *parent = nullptr);
+
+    bool workDown();
+    bool check(){return devExist();}
+
+signals:
+    void fabSig(QString str);
+
+protected:
+    bool devExist();
+    bool at91recovery();
+
+    bool createFab();
+    bool programFab();
+    int shexec(const char *cmd, char res[][512], int count);
+    QString processOn(const QString &cmd);
+    bool isFileExist(const QString &fn);
+};
+
+#endif // TEST_FBAPARTITION_H
