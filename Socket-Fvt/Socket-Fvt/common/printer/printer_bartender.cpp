9fbe9bbd47e4bbf1f320a5f1c9f5d993a3229e99
luozhiyong <luozhiyong131@qq.com>
2022-03-24 16:32:10 +0800

    sn

--- Socket-Fvt/Socket-Fvt/common/printer/printer_bartender.cpp
+++ Socket-Fvt/Socket-Fvt/common/printer/printer_bartender.cpp
@@ -9,7 +9,7 @@
 Printer_BarTender::Printer_BarTender(QObject *parent) : QObject(parent)
 {
     mSocket = new QUdpSocket(this);
-    mSocket->bind(QHostAddress::LocalHost, 47755);
+    mSocket->bind(QHostAddress::Any, 47755);
     connect(mSocket,SIGNAL(readyRead()),this,SLOT(recvSlot()));
 }
 
@@ -27,7 +27,7 @@ QString Printer_BarTender::createOrder(sBarTend &it)
     str += it.pn + ","; str += it.hw + ","; str += it.fw + ",";
     QString date = QDate::currentDate().toString("yy") + "W";
     date += QString("%1").arg(QDate::currentDate().weekNumber(), 2, 10, QLatin1Char('0'));
-    str += date+","; str += it.sn + ","; it.sn = it.sn.remove(QRegExp("\\s"));
+    str += date+","; str += "SN " + it.sn + ","; it.sn = it.sn.remove(QRegExp("\\s"));
     str += QString("G$K:%1%$S:%2%M:%3$HW:%4$FW%5").arg(it.pn).arg(it.sn).arg(date).arg(it.hw).arg(it.fw);
     return str;
 }
@@ -52,10 +52,11 @@ bool Printer_BarTender::recvResponse(int sec)
 bool Printer_BarTender::printer(sBarTend &it)
 {
     int port = 1044;
-    sendMsg("start", port);
+    QHostAddress host = QHostAddress::Broadcast;
+    sendMsg("start", port, host);
 
     QString order = createOrder(it);
-    sendMsg(order.toLocal8Bit(), port+1);
+    sendMsg(order.toLocal8Bit(), port+1, host);
     return recvResponse(3);
 }
 
